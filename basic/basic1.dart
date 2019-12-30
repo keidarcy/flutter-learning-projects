38402fdf9b41e8530accb05777c6305126c76177
XING YAHAO <keidarcy.1015@gmail.com>
2019-12-30 16:40:18 +0900

    basic1

---
+++ basic/basic1.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+      ),
+    );
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        // child: RaisedButton.icon(
+        //   onPressed: () {
+        //     print('clicked');
+        //   },
+        //   icon: Icon(Icons.mail),
+        //   label: Text('mail me'),
+        //   color: Colors.lightBlue,
+        // ),
+        child: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.alternate_email),
+          color: Colors.amber,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
